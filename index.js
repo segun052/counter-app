a519a02d84fba3edea74df4db0b379bfc1631ace
Segun <segunvictor052@gmail.com>
2024-09-20 11:40:40 +0100

    fixed the decrement bug

--- index.js
+++ index.js
@@ -2,24 +2,44 @@ let saveEl = document.getElementById("save-el")
 let countEl = document.getElementById("count-el")
 let count = 0
 
-function increment() {
-    count += 1
-    countEl.textContent = count
-}
+// function increment() {
+//     count += 1
+//     countEl.textContent = count
+// }
 
-function decrement(count) {
-    if (count > 0) {
-        count -= 1;
+// function decrement(count) {
+//     if (count > 0) {
+//         count -= 1;
 
-    } else {
-        count = 0;
-    }
+//     } else {
+//         count = 0;
+//     }
     
-    countEl.textContent = count
-}
+//     countEl.textContent = count
+// }
+
+
+function increment(){
+    count += 1
+    countEl.textContent = count}
+        
+    function decrement() {
+        if (count > 0) {
+            count -= 1;
+
+        } else {
+            count = 0;
+        }
+        
+        countEl.textContent = count
+        }
+        decrement()
+    increment()
+
+
 
 function save() {
-    let countStr = count + " - "
+    let countStr =  " - " + count 
     saveEl.textContent += countStr
     countEl.textContent = 0
     count = 0
